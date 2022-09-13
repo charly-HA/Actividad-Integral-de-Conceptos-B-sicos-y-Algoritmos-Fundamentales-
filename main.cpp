498798e1415d597c1e5e2984cec888d4c1ef6f13
charly-ha <hernandezarciniegacarlos@gamil.com>
2022-09-12 22:22:44 -0500

    Avance 1

--- main.cpp
+++ main.cpp
@@ -6,9 +6,26 @@
 //
 // =================================================================
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	ifstream input;
+	ofstream output;
+	if (argc != 3){
+		cout << "Error";
+		return -1;
+	}
+	input.open(argv[1]);
+	if(!input.is_open()){
+		cout << "El archivo no existe";
+		return -1;
+	}
+	output.open(argv[2]);
+    int nb;
+	input>>nb;
+	string ID;
+	input>>ID;
+	
 	return 0;
 }
